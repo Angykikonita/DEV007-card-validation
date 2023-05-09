519cb4a13188e690bb11272c7055c4682b6aa2a9
angy <angykikonitaphoto@gmail.com>
2023-05-09 09:10:49 -0600

    commit3

--- src/index.js
+++ src/index.js
@@ -2,28 +2,28 @@ import validator from './validator.js';
 
 
 
-const custormerName = document.getElementById('customerName');
+//const custormerName = document.getElementById('customerName');
 const cardNumber = document.getElementById('cardNumber');
 const button = document.getElementById('button');
+//const numeroMasc = validator.maskify(cardNumber);
+/*const leyendaNombre = document.getElementById();
+const personas = [];
+  personas.forEach()
+leyendaNombre.innerHTML = "Bienvenidx " + <div class="usuario">$(custormerName.value)</div>;*/
 
-document.getElementById("leyendaNombre").innerHTML = "Bienvenidx " + custormerName.value;
-
-    
 button.addEventListener("click", (e) => {
   e.preventDefault();
  
   validator.isValid(cardNumber.value);
   validator.maskify(cardNumber.value);
-  alert(" tus datos son  " + custormerName.value + "  Tu numero de tarjeta es  " + cardNumber.value);
+    
+  //alert(" tus datos son  " + custormerName.value + "  Tu numero de tarjeta es  " + digitosmascara);
+  //alert("numero de tarjeta es  " + valitador.value);
+  
 
 });
+console.log(validator.isValid)
+console.log(validator.maskify) 
 
  
 
-/*customerName.addEventListener("customerName", function(){
-  const value= customerName.value;
-  const p= document.createElement("p");
-  p.innerText =value;
-  leyendanombre.appendChild(p);
-
-});*/
